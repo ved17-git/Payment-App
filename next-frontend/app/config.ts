16371573c38c78c8ea16731b86e2ccde2461002b
Da4Rkclown <118552956+Da4Rkclown@users.noreply.github.com>
2025-12-12 10:50:15 +0530

    updated backend url

--- next-frontend/app/config.ts
+++ next-frontend/app/config.ts
@@ -1,2 +1,6 @@
-export const Deployed_URL="https://payment-backend.kozow.com"
+export const Deployed_URL="https://payment-backend-teal.vercel.app"
 export const BACKEND_URL="http://localhost:8000"
+
+//http://localhost:3000 -> local development
+//https://payment-backend-teal.vercel.app/ -> using  vercel cli
+//https://payment-backend.kozow.com ->aws deployed URL
